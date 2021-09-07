1808fbbe68933d113a5b8b35021a39d1b39fb6d6
Henry417E1610 <renyu@ualberta.ca>
2021-09-07 14:49:27 -0600

    simplified the code

--- google.py
+++ google.py
@@ -1,18 +1,17 @@
-import requests, sys
+import requests
 
 if __name__ == "__main__":
-    sys.stdout.write(f'Request Version: {requests.__version__} \n')
-    sys.stdout.write(
-        f'Google homepage: {requests.get("https://www.google.com").text}')
+    print('Request Version: '+ str(requests.__version__) +' \n')
+    print('Google homepage: '+ str(requests.get("https://www.google.com").text)+'\n')
     python_script = requests.get(
         "https://raw.githubusercontent.com/Henry417E1610/cmput404_lab1/master/google.py")
     source_code = requests.get(
             'https://github.com/Henry417E1610/cmput404_lab1/blob/master/google.py'
         )
-    sys.stdout.write(
-        f'The source code of the python script from github is: {source_code.text}\n'
+    print(
+        'The source code of the python script from github is: '+str(source_code.text)+'\n'
     )
-    sys.stdout.write(f'Raw link: {python_script.text}\n')
+    print('Raw link: '+str(python_script.text) + '\n')
     
     with open('github_script.py', 'w') as doc:
         doc.write(python_script.text)
